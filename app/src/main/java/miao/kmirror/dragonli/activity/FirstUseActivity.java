e74f6a33384c8d03576ac820a3ebf01496f35e9d
Kmirror <Kmirror@outlook.com>
2021-12-15 16:12:24 +0800

    第一次使用软件加载应用和网站跳转库

--- app/src/main/java/miao/kmirror/dragonli/activity/FirstUseActivity.java
+++ app/src/main/java/miao/kmirror/dragonli/activity/FirstUseActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.List;
+
 import miao.kmirror.dragonli.R;
+import miao.kmirror.dragonli.dao.AppPackageDao;
+import miao.kmirror.dragonli.dao.WebInfoDao;
+import miao.kmirror.dragonli.entity.AppPackage;
+import miao.kmirror.dragonli.entity.WebInfo;
 import miao.kmirror.dragonli.lock.activity.ImageLockActivity;
+import miao.kmirror.dragonli.utils.JsonUtils;
 import miao.kmirror.dragonli.utils.MD5Utils;
 import miao.kmirror.dragonli.utils.PasswordUtils;
 import miao.kmirror.dragonli.utils.SpfUtils;
@@ -16,13 +27,17 @@ import miao.kmirror.dragonli.utils.ToastUtils;
 
 public class FirstUseActivity extends AppCompatActivity {
 
+    private static final String TAG = "FirstUseActivity";
     private EditText EtPassword;
     private EditText EtAgainPassword;
+    private AppPackageDao appPackageDao = new AppPackageDao();
+    private WebInfoDao webInfoDao = new WebInfoDao();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_use);
+        initLocalWebAndApp();
         EtPassword = findViewById(R.id.et_password);
         EtAgainPassword = findViewById(R.id.et_again_password);
         Button toSettingImage = findViewById(R.id.to_setting_image);
@@ -37,5 +52,21 @@ public class FirstUseActivity extends AppCompatActivity {
                 ToastUtils.toastShort(this, "两次密码不一致请重新输入！！！");
             }
         });
+    }
+
+    private void initLocalWebAndApp() {
+        if (appPackageDao.findAll().size() == 0 && webInfoDao.findAll().size() == 0) {
+            Gson gson = new Gson();
+            List<AppPackage> appPackages = gson.fromJson(
+                    JsonUtils.getJson("app.json", this),
+                    new TypeToken<List<AppPackage>>() {
+                    }.getType());
+            appPackageDao.saveAll(appPackages);
+            List<WebInfo> webInfoList = gson.fromJson(
+                    JsonUtils.getJson("web.json", this),
+                    new TypeToken<List<WebInfo>>() {
+                    }.getType());
+            webInfoDao.saveAll(webInfoList);
+        }
     }
 }
